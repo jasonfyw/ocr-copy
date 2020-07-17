b3d37f7d1a39a1f004da2894fdb4a6054f47813c
Jason Wang <jasonwang0610@gmail.com>
2020-07-17 15:35:02 +0200

    Fixed problems and added preprocessing

--- ocrcopy/ocrcopy.py
+++ ocrcopy/ocrcopy.py
@@ -2,6 +2,8 @@ from pynput import mouse, keyboard
 import pyscreenshot
 import pytesseract
 import pyperclip
+import numpy as np
+from PIL import ImageOps, ImageEnhance
 
 class OCRCopy():
     def __init__(self):
@@ -17,21 +19,46 @@ class OCRCopy():
             self.x2, self.y2 = x, y
             return False
 
-    def get_screenshot(self, bbox):
+    def get_screenshot(self, x1, y1, x2, y2):
+        if x2 >= x1 and y2 <= y1:
+            bbox = (x1, y2, x2, y1)
+        elif x2 <= x1 and y2 <= y1:
+            bbox = (x2, y2, x1, y1)
+        elif x2 <= x1 and y2 >= y1:
+            bbox = (x2, y1, x1, y2)
+        else: 
+            bbox = (x1, y1, x2, y2)
         img = pyscreenshot.grab(bbox = bbox)
         return img
 
-    def copy_to_clipboard(self, img):
-        text = pytesseract.image_to_string(img)
-        pyperclip.copy(text)
+    def preprocess_image(self, img):
+        img = img.convert('L') 
+
+        # print(np.mean(img))
+        if np.mean(img) < 100:
+            img = ImageOps.invert(img)
+
+        img = ImageEnhance.Contrast(img).enhance(1.5)
+
+        return img
+
+    def recognise_text(self, img):
+        config = r'--oem 3 -l eng'
+        text = pytesseract.image_to_string(img, config = config)
+
+        return text
 
     def activate_shortcut(self):
         self.x1, self.x2, self.y1, self.y2 = 0, 0, 0, 0
         with mouse.Listener(on_click = self.on_click) as listener:
             listener.join()
 
-        img = self.get_screenshot((self.x1, self.y1, self.x2, self.y2))
-        self.copy_to_clipboard(img)
+        img = self.get_screenshot(self.x1, self.y1, self.x2, self.y2)
+        img = self.preprocess_image(img)
+        text = self.recognise_text(img)
+
+        pyperclip.copy(text)
+        # img.show()
     
 
 if __name__ == "__main__":
