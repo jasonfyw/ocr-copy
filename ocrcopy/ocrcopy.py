566010a58303457be5ca7b86c8c7fa134ee635b6
Jason Wang <jasonwang0610@gmail.com>
2020-07-17 21:14:34 +0200

    Create overlay class

--- ocrcopy/ocrcopy.py
+++ ocrcopy/ocrcopy.py
@@ -4,6 +4,91 @@ import pytesseract
 import pyperclip
 import numpy as np
 from PIL import ImageOps, ImageEnhance
+import pyglet
+import tkinter as tk
+
+
+class Overlay():
+    def __init__(self, root, width, height, x, y):
+        self.width, self.height = width, height
+        self.x, self.y = x, y
+
+        self.overlay = tk.Toplevel(root)
+        self.frame = tk.Frame(self.overlay)
+        self.frame.pack(fill = tk.BOTH, expand = tk.YES)
+
+        self.rect = None
+        self.rect_startx = None
+        self.rect_starty = None
+
+        self.configure_canvas()
+        self.configure_overlay()
+
+    def configure_canvas(self):
+        self.canvas = tk.Canvas(self.frame, cursor = 'cross', bg = '#ffffff')
+        self.canvas.pack(fill = tk.BOTH, expand = tk.YES)
+
+        self.canvas.bind('<ButtonPress-1>', self.handle_button_press)
+        self.canvas.bind('<B1-Motion>', self.handle_move_press)
+        self.canvas.bind('<ButtonRelease-1>', self.handle_button_release)
+
+    def configure_overlay(self):
+        geometry = '{}x{}+{}+{}'.format(self.width, self.height, self.x, self.y)
+
+        self.overlay.bind_all('<Configure', )
+        self.overlay.geometry(geometry)
+        self.overlay.attributes('-transparent', True)
+        self.overlay.attributes('-alpha', 0.1)
+        self.overlay.lift()
+        self.overlay.attributes('-topmost', True)
+
+        self.overlay.bind('<Configure>', lambda e: self.overlay.geometry(geometry)) 
+        self.overlay.bind('<ButtonPress-1>', lambda e: print(e.x_root, e.y_root))   
+
+    """
+    Handle Canvas input events
+    """
+    def handle_button_press(self, event):
+        self.rect_startx = self.canvas.canvasx(event.x)
+        self.rect_starty = self.canvas.canvasy(event.y)
+
+        self.rect = self.canvas.create_rectangle(
+            self.rect_startx, 
+            self.rect_starty, 
+            self.rect_startx, 
+            self.rect_starty, 
+            outline = '#ff0000',
+            width = 2,
+            fill = '#bbbbbb'
+        )
+
+    def handle_move_press(self, event):
+        self.canvas.coords(self.rect, self.rect_startx, self.rect_starty, event.x, event.y)
+
+    def handle_button_release(self, event):
+        self.canvas.delete(self.rect)
+
+class OverlayController():
+    def __init__(self):
+        self.root = tk.Tk()
+        self.root.geometry('0x0')
+        self.root.withdraw()
+
+        self.overlays = []
+        self.create_overlays()
+
+        self.root.mainloop()
+
+    def create_overlays(self):
+        for i, screen in enumerate(pyglet.window.get_platform().get_default_display().get_screens()):
+            x, y, w, h = screen.x, screen.y, screen.width, screen.height
+            self.overlays.append(Overlay(self.root, w, h, x, y))
+
+    def destroy(self):
+        for overlay in self.overlays:
+            overlay.overlay.destroy()
+            self.root.destroy()
+
 
 class OCRCopy():
 
@@ -73,4 +158,5 @@ class OCRCopy():
 
 
 if __name__ == "__main__":
-    ocrcopy = OCRCopy()
+    # ocrcopy = OCRCopy()
+    thing = OverlayController()
