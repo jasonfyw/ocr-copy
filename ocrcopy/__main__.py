568083c5deef51ed578c86136b8ba2701dfb4f8a
Jason Wang <jasonwang0610@gmail.com>
2020-07-24 21:28:50 +0200

    Changed lang code

--- ocrcopy/__main__.py
+++ ocrcopy/__main__.py
@@ -4,7 +4,7 @@ from ocrcopy.controller import Controller
 
 def main():
     parser = argparse.ArgumentParser(description = "A program that uses OCR to copy text from a screen selection")
-    parser.add_argument('-l', nargs = '?', const = 'eng+ara+chi-sim+rus+ell+jpn+hin', default = 'eng', help = 'Languages to be checked for by the OCR engine, separated by "+". Available language codes at https://github.com/tesseract-ocr/tessdata. See README for more details')
+    parser.add_argument('-l', nargs = '?', const = 'eng+ara+chi_sim+rus+ell+jpn+hin', default = 'eng', help = 'Languages to be checked for by the OCR engine, separated by "+". Available language codes at https://github.com/tesseract-ocr/tessdata. See README for more details')
 
     args = parser.parse_args()
 
